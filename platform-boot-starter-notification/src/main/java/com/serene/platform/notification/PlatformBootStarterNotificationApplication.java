2b919e90b75aa0c37ed0e90e394e62d4ac388cc6
tongbin102 <tongbin102@126.com>
2024-05-15 14:49:28 +0800

    1

---
+++ platform-boot-starter-notification/src/main/java/com/serene/platform/notification/PlatformBootStarterNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.serene.platform.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformBootStarterNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformBootStarterNotificationApplication.class, args);
+    }
+
+}
